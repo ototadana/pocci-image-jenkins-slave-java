13e017f40826df6a924209e373df9278d501370f
ototadana <ototadana@gmail.com>
2016-10-15 14:51:59 +0000

    Change the behavior to create settings.xml by MAVEN_REPOSITORY_URL

--- check-update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd `dirname $0`
-
-LATEST=`curl -s http://www.us.apache.org/dist/maven/maven-3/ |grep "\[DIR\]" |sed -E 's/^.+<a href=.+\/">(.+)\/<\/a> +(....-..-.. ..:..)(.+)/\2 \1/g' |sort -r |head -1 |cut -d" " -f3`
-CURRENT=`grep "ENV MVN_VERSION" Dockerfile |cut -d" " -f3`
-if [ ${LATEST} != ${CURRENT} ]; then
-    echo "MVN_VERSION: ${CURRENT} --> ${LATEST}"
-fi
